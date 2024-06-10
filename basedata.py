cf6e8b0e5fa7dfde61e35a02c142770d7b2c2c3e
Bigrobotmaker <107852313+Bigrobotmaker@users.noreply.github.com>
2024-06-10 15:40:28 +0100

    summary

--- basedata.py
+++ basedata.py
@@ -3,6 +3,7 @@ going = True
 operation = 'a'
 submitname = 'a'
 submitscore = 0
+searcher = ''
 connection = sqlite3.connect("scoreboard.db")
 cursor = connection.cursor()
 cursor.execute("CREATE TABLE IF NOT EXISTS scoreboard (name TEXT, score TEXT)")
@@ -13,6 +14,14 @@ while going == True:
         submitscore = str(input('what was your score? '))
         cursor.execute('INSERT INTO scoreboard VALUES ("' + submitname + '", "' + submitscore +'")')
         connection.commit()
+    if operation == 'search':
+        searcher = str(input("who's score would you like to search for?"))
+        cursor.execute('SELECT * FROM scoreboard\nWHERE name = "' + searcher + '"')
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
     if operation == 'exit':
         going = False
+    else:
+        print('operation not recognised')
 connection.close()
